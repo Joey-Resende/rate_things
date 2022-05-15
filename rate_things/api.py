5c4d837d25cac2347d010b0278e0a0a1783dbc62
Joey-Resende <givanildo.resende@gmail.com>
2022-05-15 15:54:18 -0300

    Adicionando o serializer

--- rate_things/api.py
+++ rate_things/api.py
@@ -1,10 +1,22 @@
+from typing import List
 from fastapi import FastAPI
 from rate_things.core import get_things_from_database
+from rate_things.serializers import ThingsIn, ThingsOut
 
 api = FastAPI(title="rate_things")
 
 
-@api.get('/things/')
+@api.get('/things/', response_model=List[ThingsOut])
 def list_things():
     things = get_things_from_database()
     return things
+
+
+@api.post('/things/', response_model=ThingsIn)
+def add_things(things_in: ThingsIn):
+    things = Thing(**things_in.dict())
+    with get_session() as session:
+        session.add(things)
+        session.commit()
+        session.refresh(things)
+        return things
---
+++ rate_things/serializers.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from pydantic import BaseModel, validator
+from fastapi import HTTPException, status
+
+
+class ThingsOut(BaseModel):
+    id: int
+    things: str
+    name: str
+    gender: str
+    score: int
+    image: int
+    cost: int
+    rate: int
+    date: datetime
+
+
+class ThingsIn(BaseModel):
+    things: str
+    name: str
+    gender: str
+    score: int
+    image: int
+    cost: int
+
+    @validator('score', 'image', 'cost')
+    def validate_ratings(cls, v, field):
+        if v < 1 or v > 10:
+            raise HTTPException(
+                detail=f'{field.name} must be between 1 and 10', status_code=status.HTTP_400_BAD_REQUEST)
+        return v
