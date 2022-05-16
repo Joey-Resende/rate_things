9e33dcf600e8a3491c206f1c80984e0321ac6cf1
Joey-Resende <givanildo.resende@gmail.com>
2022-05-16 12:16:37 -0300

    Ajustando o serializer e a api

--- rate_things/api.py
+++ rate_things/api.py
@@ -1,5 +1,5 @@
-from typing import List
-from fastapi import FastAPI
+from typing import List, optional
+from fastapi import FastAPI, Response, status
 from rate_things.core import get_things_from_database
 from rate_things.serializers import ThingsIn, ThingsOut
 from rate_things.database import get_session
@@ -9,16 +9,18 @@ api = FastAPI(title="rate_things")
 
 
 @api.get('/things/', response_model=List[ThingsOut])
-def list_things():
-    things = get_things_from_database()
+async def list_things(style: Optional[str] = None):
+    things = get_things_from_database(style)
     return things
 
 
 @api.post('/things/', response_model=ThingsIn)
-def add_things(things_in: ThingsIn):
+async def add_things(things_in: ThingsIn, response: Response):
     things = Thing(**things_in.dict())
     with get_session() as session:
         session.add(things)
         session.commit()
         session.refresh(things)
-        return things
+
+    response.status_code = status.HTTP_201_CREATED
+    return things
--- rate_things/serializers.py
+++ rate_things/serializers.py
@@ -27,5 +27,6 @@ class ThingsIn(BaseModel):
     def validate_ratings(cls, v, field):
         if v < 1 or v > 10:
             raise HTTPException(
-                detail=f'{field.name} must be between 1 and 10', status_code=status.HTTP_400_BAD_REQUEST)
+                detail=f'{field.name} must be between 1 and 10', status_code=status.HTTP_400_BAD_REQUEST
+            )
         return v
